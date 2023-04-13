fe7b454b301941f7921812b816e32259da9c6961
Marie-Ève Pelletier <e2142821@cmaisonneuve.qc.ca>
2023-04-13 13:03:50 -0400

    S11C1 Avancement carrousel

--- js/carrousel.js
+++ js/carrousel.js
@@ -2,9 +2,42 @@
     {
         console.log('début carrousel');
         let carrousel__ouvrir = document.querySelector(".carrousel__ouvrir");
+        let carrousel__x = document.querySelector(".carrousel__x");
         let carrousel = document.querySelector(".carrousel");
+        let carrousel__figure = document.querySelector(".carrousel__figure");
+        console.log(carrousel__figure.tagName);
+        let galerie = document.querySelector(".galerie");
+        let galerie__img = galerie.querySelectorAll("img");
+
+        console.log(galerie__img.length);
+
+
+
         carrousel__ouvrir.addEventListener('mousedown', function(){
             carrousel.classList.add("carrousel--ouvrir")
+            ajouter_img_dans_carrousel();
         })
+
+        carrousel__x.addEventListener('mousedown', function(){
+            carrousel.classList.remove("carrousel--ouvrir")
+        })
+
+        function ajouter_img_dans_carrousel() {
+            for(const elm of galerie__img) {
+                // console.log(elm.getAttribute("src"));
+                let img = document.createElement("img");
+                img.setAttribute("src", elm.getAttribute("src"));
+                carrousel__figure.appendChild(img);
+                console.log(img.getAttribute("src"));
+            }
+
+            // for (let i = 0; i < galerie__img.length; i++) {
+
+            //     let img = document.createElement("img");
+            //     img.setAttribute("src", galerie__img[i].src);
+            //     carrousel__figure.appendChild(img);
+                
+            // }
+        }
     }
 )()

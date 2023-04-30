d39f53c3efea97686564c50ac3b6f66148b68e83
marieeve0916 <e2142821@cmaisonneuve.qc.ca>
2023-04-30 14:18:03 -0400

    Modifications du JavaScript

--- js/carrousel.js
+++ js/carrousel.js
@@ -5,18 +5,20 @@
         let carrousel__x = document.querySelector(".carrousel__x");
         let carrousel = document.querySelector(".carrousel");
         let carrousel__figure = document.querySelector(".carrousel__figure");
-        console.log(carrousel__figure.tagName);
+        let carrousel__form = document.querySelector(".carrousel__form");
+
         let galerie = document.querySelector(".wp-block-gallery");
         let galerie__img = galerie.querySelectorAll("img");
-        let carrousel__form = document.querySelector(".carrousel__form");
+        // console.log(galerie__img.length);
+        
         let index = 0;
         let ancien_index = -1;
         let position = 0;
-        console.log(galerie__img.length);
 
 
 
         carrousel__ouvrir.addEventListener('mousedown', function(){
+            index = 0;
             carrousel.classList.add("carrousel--ouvrir")
             ajouter_img_dans_carrousel();
         })
@@ -25,24 +27,29 @@
             carrousel.classList.remove("carrousel--ouvrir")
         })
 
+/* -- boucle qui permet construire le carrousel */
+
     /** 
     * ajouter_img_dans_carrousel
     * Ajouter l'ensemble des images de la galerie dans la boîte modale carrousel
     */
-    function ajouter_img_dans_carrousel()
-    {
-   
+
         for (const elm of galerie__img) {
             elm.dataset.index = position
-            elm.addEventListener('mousedown',function(){
-            index = this.dataset.index
-            afficher_image(index)
-            console.log(index)
+            /* en cliquant sur une image de la galerie */
+            elm.addEventListener('mousedown', function(e){
+                /*
+                avant d'ouvrir la boîte modale il faut vérifier si elle n'est pas déjà ouverte
+                https://www.javascripttutorial.net/dom/css/check-if-an-element-contains-a-class/
+                la fonction contains() vous permettra de faire cette vérification
+                */
+                index = e.target.dataset.index
+                afficher_image(index)
             })
-                creation_img_carrousel(elm)
-                creation_radio_carrousel()
+            creation_img_carrousel(elm)
+            creation_radio_carrousel()
         }
-    }
+
  
     function creation_img_carrousel(elm){
         //console.log(elm.getAttribute('src'))
@@ -64,9 +71,7 @@
         rad.setAttribute('name', 'carrousel__rad')
         rad.classList.add('carrousel__rad')
         rad.dataset.index = position
-        position = position + 1 // incrémentation de 1
-        // position += 1
-        // position++
+        position++
         carrousel__form.appendChild(rad)
         rad.addEventListener('mousedown', function(){
             console.log(this.dataset.index)
